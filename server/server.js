c9b33897cdebf97f3a846ad04ce97c61ba1f61fd
Rob Peschke <rspesc31@gmail.com>
2020-06-10 16:53:03 -0400

    moved server into its own file

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+// app.use("/api/workouts", require("./routes/workouts"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
